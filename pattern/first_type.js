93006a174276458d81fd5b90f700df6b3b995e49
krishna146804 <krishnayadav5967@gmaiil.com>
2025-06-30 11:32:07 +0530

    patternn

---
+++ pattern/first_type.js
@@ -0,0 +1,10 @@
+function pattern() {
+    for (let i = 0; i < 4; i++) {
+        for (let j = 0; j < 4; j++) {
+            
+            process.stdout.write("8 ");       }// print the 8 in the row
+        console.log(); // to move to the next line after each row
+    }
+}
+
+pattern();
