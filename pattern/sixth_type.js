80b3228f55fe5f27b8e8f958d9108aac99444bb9
krishna146804 <krishnayadav5967@gmail.com>
2025-07-05 09:26:00 +0530

    six type

---
+++ pattern/sixth_type.js
@@ -0,0 +1,12 @@
+const pattern = function (n){
+    for (let i=1; i<=n; i++){
+        for(let j=1; j<=n-i+1; j++){
+            process.stdout.write(j + " ");        
+        }
+        console.log();
+
+    }
+    
+}
+
+pattern(5);
